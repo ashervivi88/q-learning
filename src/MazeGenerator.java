d7d5d331dce59fb1e8deb0edf92164fdf267aa6c
Ashley Kim <akim20@akim20-3002.local>
2020-02-25 11:15:12 -0700

    debugged mazeGenerator

--- src/MazeGenerator.java
+++ src/MazeGenerator.java
@@ -1,17 +1,20 @@
+import java.io.*;
 import java.util.Random;
 
 public class MazeGenerator {
-    public void generate(int numRuns){
-        // create instance of Random class
+    public void generate(int numRuns) throws IOException {
 
         Random rand = new Random();
+
+        for(int i = 0; i< numRuns; i++){
+
         int[][] m = new int[3][3];
         int xRow = 0;
         int xCol = 0;
         int fRow = 0;
         int fCol = 0;
 
-        while(xRow == fRow && xCol == fCol) {
+        while (xRow == fRow && xCol == fCol) {
             xRow = rand.nextInt(3);
             xCol = rand.nextInt(3);
             fRow = rand.nextInt(3);
@@ -20,11 +23,23 @@ public class MazeGenerator {
 
         m[xRow][xCol] = 1;
         m[fRow][fCol] = 2;
+        String output = ""+m[0][0] + " " + m[0][1] + " " + m[0][2] + "\n" +
+                        m[1][0] + " " + m[1][1] + " " + m[1][2] + "\n" +
+                        m[2][0] + " " + m[2][1] + " " + m[2][2];
 
+        writeFile(output, i);
+    }
+    }
 
+        public void writeFile(String output, int fileNum)
+  throws IOException {
+            String fileName = "maze"+fileNum+".txt";
+            BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
+            writer.write(output);
+            writer.close();
+        }
 
 
 
     }
 
-}
--- src/QMazeDriver.java
+++ src/QMazeDriver.java
@@ -1,2 +1,13 @@
+import java.io.IOException;
+
 public class QMazeDriver {
+    public static void main(String[]args) throws IOException {
+
+        MazeGenerator mg = new MazeGenerator();
+        mg.generate(5);
+
+
+
+
+    }
 }
